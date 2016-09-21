9b15c0dbda1425885bb565b493e2f99edb5441f6
amberylx <amberyl.x@gmail.com>
2016-09-21 16:45:51 -0700

    Add setup.py for package distribution

---
+++ setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+
+install_requires = [
+    'django >=1.7, <2',
+    'django-cors-headers',
+    'djangorestframework',
+    'fake-factory',
+    'inflection',
+    'jsondiff',
+    'requests',
+    'simplejson',
+    'urllib3[secure]',
+]
+
+setup(
+    name='jsonapi-mock-server',
+    description='JSON API mock server',
+    version='0.1',
+    author='ZeroCater',
+    packages=find_packages(),
+    install_requires=install_requires,
+    licence='MIT',
+    url='https://github.com/ZeroCater/mp-mock-server',
+    keywords='jsonapi',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
